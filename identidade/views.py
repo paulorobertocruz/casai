e24140d5dfa87c53d903351d80752cce59a8aafe
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-01-18 10:13:18 -0300

    atualiza pagina de pesquisa de usuario

--- identidade/views.py
+++ identidade/views.py
@@ -6,11 +6,12 @@ def usuario(request):
     template_name = "identidade/usuario.html"
     context = {}
     
+    numero_prontuario = request.GET.get("numero_prontuario", None)
+
     nome = request.GET.get("nome", None)
     nome_mae = request.GET.get("nome_mae", None)
     sexo = request.GET.get("sexo", None)
     
-
     context["etnias"] = Etnia.objects.all()
     context["aldeias"] = Aldeia.objects.all()
     
@@ -19,19 +20,27 @@ def usuario(request):
     
     usuarios = Usuario.objects.all()
     
+    filtered = False
 
-    if nome is not None:
+    if numero_prontuario is not None and numero_prontuario is not "":
+        usuarios = usuarios.filter(prontuario__numero = numero_prontuario)
+        filtered = True
+
+    if nome is not None and nome is not "":
         usuarios = usuarios.filter(nome__icontains = nome)
         filtered = True
     
-    if nome_mae is not None:
+    if nome_mae is not None and nome_mae is not "":
         usuarios = usuarios.filter(nome_mae__icontains = nome_mae)
         filtered = True
 
     if sexo is not None:
         usuarios = usuarios.filter(sexo = sexo)
         filtered = True
-    
+
+    if not filtered:
+        usuarios = None
+
     context["usuarios"] = usuarios
 
     return render(request, template_name, context)
